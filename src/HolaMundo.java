da30bdd3b352fac3f36d5d57dbe35187fe3245dd
Rutbel Carlos <rttitoca@unsa.edu.pe>
2022-04-28 20:15:45 -0500

    agregando nombre del desarrollador

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Bienvenidos al curso de Pweb2");
+        System.out.println("Atte: Rutbel Carlos Ttito Campos");
     }
 }
